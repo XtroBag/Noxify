cb1ad35db2ff38c8711d64164ba4090580f4f348
Sean Gray <scgray2006@gmail.com>
2024-12-26 18:08:25 -0500

    new banner

--- src/commands/chat/message.ts
+++ src/commands/chat/message.ts
@@ -2,11 +2,11 @@ import { Message } from "discord.js";
 import { CommandTypes, MessageCommandModule } from "../../handler";
 
 export = {
-    name: "Hello",
-    type: CommandTypes.MessageCommand,
-    disabled: false,
-    ownerOnly: false,
-    async execute(client, message: Message): Promise<void> {
-        await message.reply(`Hello <@${message.author.id}>`);
+  name: "Hello",
+  type: CommandTypes.MessageCommand,
+  disabled: false,
+  ownerOnly: false,
+  async execute(client, message: Message): Promise<void> {
+    await message.reply(`Hello <@${message.author.id}>`);
     }
 } as MessageCommandModule;
